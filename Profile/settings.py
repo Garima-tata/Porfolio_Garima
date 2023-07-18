ecfbac60f04b59d9b984ca4ea0b0f97aad97ad4f
Garima Tata <garimat@iitbhilai.ac.in>
2023-07-18 11:38:42 +0530

    database

--- Profile/settings.py
+++ Profile/settings.py
@@ -77,16 +77,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Profile.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'profiledb',
+        'USER': 'root',
+        'PASSWORD': 'Anmol@2004',
+        'HOST': 'localhost',  # Change this to your MySQL server's host if necessary
+        'PORT': '3306',       # Change this to your MySQL server's port if necessary
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
